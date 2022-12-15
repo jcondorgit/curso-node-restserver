ff217aee35faa5462e45902ba2cc16b61a3e07c1
jcondorgit <jonnathancondor90@gmail.com>
2022-12-15 12:27:26 -0500

    Fin sección 11 - version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response, request } = require('express');
 const bcrypt = require('bcrypt');
 const UsuarioModelo = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt-generador');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async(req = request, res = response) => {
@@ -61,6 +62,64 @@ const login = async(req = request, res = response) => {
 
 }
 
+const googleSignIn = async(req, res = response)=>{
+
+    const { id_token } = req.body;
+
+    try {
+        
+        const {nombre, img, correo} = await googleVerify( id_token );
+
+        let usuario = await UsuarioModelo.findOne( {correo} );
+
+        if ( !usuario ) {
+            //se crea un nuevo usuario
+
+            const data ={
+                nombre, 
+                correo,
+                password: '123',
+                img,
+                google: true,
+                estado: true,
+                rol: 'USER_ROLE'
+            }
+
+            usuario = new UsuarioModelo(data);
+            await usuario.save();
+
+        }
+
+        //Si el estado del usuario está en false
+        if ( !usuario.estado ) {
+                
+            return res.status(401).json({
+                msg:'El usuario está desactivado, contacte al administrador'
+            })
+
+        }
+        //Generar el JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        })
+
+    } catch (error) {
+        
+        console.log(error);
+        res.status(400).json({
+            ok:'false',
+            msg:'El token no se pudo validar'
+        })
+
+    }
+
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
